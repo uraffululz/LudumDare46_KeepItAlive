a5e9019991f9548f2050a8c1671ad07b5bd4b531
Unknown <randomsimpson@gmail.com>
2020-04-17 20:42:02 -0500

    Beginnings
    Basic environment. Player moves. Camera follows.

---
+++ KeepItAlive/Assets/CameraScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+	[SerializeField] GameObject player;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+		Vector3 newCameraPos = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        transform.position = newCameraPos;
+    }
+}
---
+++ KeepItAlive/Assets/PlayerMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour{
+
+	[SerializeField] GameObject player;
+
+	[SerializeField] float hSpeed;
+	[SerializeField] float vSpeed;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+
+	void Move() {
+		if (Input.GetAxisRaw("Horizontal") != 0) {
+			Vector3 newPos = new Vector3(player.transform.position.x + (Input.GetAxisRaw("Horizontal") * hSpeed), player.transform.position.y, player.transform.position.z);
+			player.transform.position = newPos;
+		}
+
+		if (Input.GetAxisRaw("Vertical") != 0) {
+			Vector3 newPos = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z + (Input.GetAxisRaw("Vertical") * vSpeed));
+			player.transform.position = newPos;
+		}
+
+	}
+}
